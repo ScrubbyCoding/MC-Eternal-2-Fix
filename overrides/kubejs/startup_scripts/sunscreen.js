eb3f4d18794ab40e2a739e138d43ca2a6b9a0c58
adam <57104144+adam9899@users.noreply.github.com>
2025-08-27 17:46:06 +0100

    Update sunscreen.js

--- overrides/kubejs/startup_scripts/sunscreen.js
+++ overrides/kubejs/startup_scripts/sunscreen.js
@@ -6,6 +6,6 @@ StartupEvents.registry('item', event => {
             food.hunger(0)
             food.saturation(0.0)
             food.effect('mowziesmobs:sunblock', 2250, 0, 1.0)
-            food.effect('vampirism:sunscreen', 2250, 0, 2.0)
+            food.effect('vampirism:sunscreen', 2250, 0, 4.0)
         })
 })
